4e34b8d6713756ea0acdf9367586e16216957a13
RafaelL1ma <lima.rafael67@hotmail.com>
2021-05-03 18:37:39 -0300

    Commit inicial StockManager

---
+++ src/main/java/br/com/StockManager/StockManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.StockManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagerApplication.class, args);
+	}
+
+}
