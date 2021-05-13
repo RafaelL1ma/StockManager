bfc09bdca9550d691bc3972ed3451401bf12dc68
RafaelL1ma <lima.rafael67@hotmail.com>
2021-05-12 23:58:58 -0300

    Criação StockManager.css, navbar,aleterações no css de lista, cadastro e edição de usuário

--- src/main/java/br/com/StockManager/DataConfiguration.java
+++ src/main/java/br/com/StockManager/DataConfiguration.java
@@ -18,7 +18,7 @@ public class DataConfiguration {
 	        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 	        dataSource.setUrl("jdbc:mysql://localhost:3306/stock_db?useTimezone=true&serverTimezone=UTC&createDatabaseIfNotExist=true");
 	        dataSource.setUsername("root");
-	        dataSource.setPassword("icasa12345");
+	        dataSource.setPassword("balisacar96382980");
 	        return dataSource;
 	    }
 
